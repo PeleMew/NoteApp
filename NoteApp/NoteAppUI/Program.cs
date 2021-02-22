9d7184dca34268e14d82f20f960c870e9ba0736d
Никита Маковский <makovskiynd@yandex.com>
2021-02-22 20:10:28 +0700

    Переименован файл Form1 в MainForm

--- NoteApp/NoteAppUI/Program.cs
+++ NoteApp/NoteAppUI/Program.cs
@@ -18,7 +18,7 @@ namespace NoteAppUI
             Note abc = new Note();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
