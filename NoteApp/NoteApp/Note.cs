ac300dd9b04607558358f12e4df46569628cb492
Никита Маковский <makovskiynd@yandex.com>
2021-02-22 22:10:19 +0700

    Доделана реализация класса Note

--- NoteApp/NoteApp/Note.cs
+++ NoteApp/NoteApp/Note.cs
@@ -26,16 +26,80 @@ namespace NoteApp
                 else
                 {
                     _name = value;
+                    _lastChangeTime = DateTime.Now;
                 }
             }
         }
 
         private NoteCategory _category;
 
+        public NoteCategory Category
+        {
+            get
+            {
+                return _category;
+            }
+
+            set
+            {
+                _category = value;
+                _lastChangeTime = DateTime.Now;
+            }
+        }
+
         private string _text;
+
+        public string Text
+        {
+            get
+            {
+                return _text;
+            }
+
+            set
+            {
+                _text = value;
+                _lastChangeTime = DateTime.Now;
+            }
+        }
 
         private DateTime _creationTime;
 
+        public DateTime CreationTime
+        {
+            get
+            {
+                return _creationTime;
+            }
+
+            set
+            {
+                _creationTime = value;
+            }
+        }
+
         private DateTime _lastChangeTime;
+
+        public DateTime LastChangeTime
+        {
+            get
+            {
+                return _lastChangeTime;
+            }
+
+            set
+            {
+                _lastChangeTime = value;
+            }
+        }
+
+        public Note(string name, NoteCategory category, string text)
+        {
+            Name = name;
+            Category = category;
+            Text = text;
+            CreationTime = DateTime.Now;
+            LastChangeTime = DateTime.Now;
+        }
     }
 }
