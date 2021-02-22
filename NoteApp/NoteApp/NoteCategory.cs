67e895e1d57fa3b6c281f04a6dc4ca6beb6d16af
Никита Маковский <makovskiynd@yandex.com>
2021-02-22 20:23:21 +0700

    Созданы классы бизнес логики

---
+++ NoteApp/NoteApp/NoteCategory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteApp
+{
+    enum NoteCategory
+    {
+        Work,
+        Home,
+        HealthAndSport,
+        People,
+        Documents,
+        Finance,
+        Other
+    }
+}
